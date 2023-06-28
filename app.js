1902b114c8487ab16d84cfe969ab1b7b200a42d6
danh.dinh <danh.dinh@s3corp.com.vn>
2023-06-28 14:07:37 +0700

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,16 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// Implement CORS
+// Access-Control-Allow-Origin *
+// cors is a middleware
+app.use(cors());
+// app.use(cors({ origin: 'https://www.natours.com' }));
+
+// * means all routes
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
